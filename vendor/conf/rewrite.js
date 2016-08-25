b32944551e6068aca9754c173332bf803d137ab5
zhanghao <jsyczhanghao@sina.com>
2016-08-25 11:50:56 +0800

    commit

--- vendor/conf/rewrite.js
+++ vendor/conf/rewrite.js
@@ -1,4 +1,7 @@
 module.exports = {
     '^/(?:\\?main|$)': '/index.html', // xxx.com | xxx.com/ | xxx.com?main => index.html
-    '^/ajax/test$': '/data/ajax/test.json'
+    '^/ajax/test$': '/data/ajax/test.json',
+    '^/ajax/callback': function(req, res){  //可以是一个函数
+        res.send('hello, world!');
+    }
 }
