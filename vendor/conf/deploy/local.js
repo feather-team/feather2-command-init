67b247e6b50ae7af9078a93d3467e07005eae5e5
zhanghao <jsyczhanghao@sina.com>
2016-08-30 17:42:27 +0800

    init时新增内容

--- vendor/conf/deploy/local.js
+++ vendor/conf/deploy/local.js
@@ -1,3 +1,4 @@
 module.exports = {
-    
+    from: '/',
+    to: '../dest'  //相对于项目根目录
 };
--- vendor/conf/rewrite.js
+++ vendor/conf/rewrite.js
@@ -1,7 +1,7 @@
 module.exports = {
     '^/(?:\\?main|$)': '/index.html', // xxx.com | xxx.com/ | xxx.com?main => index.html
-    '^/ajax/test$': '/data/ajax/test.json',
+    '^/ajax$': '/pagelet/ajax/ajax.html', //项目中的文件
     '^/ajax/callback': function(req, res){  //可以是一个函数
         res.send('hello, world!');
     }
-}
+};
